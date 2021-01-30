a1fa75f0f53e07e35d1509474c1caea42aa5a154
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2021-01-31 02:33:43 +0700

    Update poisson.py
    poission

--- poisson.py
+++ poisson.py
@@ -1,4 +1,12 @@
 from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+"""
 #ความหน้าจะเป็นในการกินข้าว2ครั้งจะมากกว่า2ครั้งหรือไม่
 x = random.poisson(lam=2,size=10)
 print(x)
+"""
+
+sns.displot(random.poisson(lam=2,size=100))
+plt.show()
